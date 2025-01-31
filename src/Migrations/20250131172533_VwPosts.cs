d630ce2038d0a1f6b383cfe6d102880c9ab9119a
Felipe Lambert <felipe.lambert@outlook.com>
2025-01-31 17:35:30 +0000

    add views migration

---
+++ src/Migrations/20250131172533_VwPosts.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class VwPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"            
+                CREATE OR REPLACE VIEW public.Posts
+                AS 
+                select
+                    b.""BlogId"" ,
+                    b.""Url"",
+                    p.""PostId"",
+                    p.""Title"" ,
+                    c.""CategoryId"",
+                    c.""Name"" as ""CategoryName""
+                from
+                    public.""Blogs"" b
+                inner join public.""Posts"" p on
+                    b.""BlogId"" = p.""BlogId""
+                inner join category.""Category"" c on
+                    b.""CategoryId"" = c.""CategoryId""
+                ;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop view public.vw_posts");
+        }
+    }
+}
